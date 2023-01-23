b7a874a3c323bb9f61e536d73071a0e665f59a53
miguel-kjh <miguelangel.kjh@gmail.com>
2023-01-23 14:44:22 +0000

    add MLP

---
+++ models/MLP.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+from utils import GELU
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = GELU(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
